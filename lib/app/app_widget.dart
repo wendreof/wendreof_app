5e12211b42071982b7504b82b15ce9f8ae672eba
wlf <wendreolf@gmail.com>
2023-01-16 21:20:53 -0300

    Refatoração

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -8,9 +9,16 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       scrollBehavior: AppScrollBehavior(),
-      title: 'Flutter Slidy',
+      title: 'Wendreof App',
       theme: ThemeData(primarySwatch: Colors.blue),
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [const ResponsiveBreakpoint.autoScale(600)],
+          background: Container(color: const Color(0xFFF5F5F5))),
     ).modular();
   }
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -46,8 +45,7 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
             mode: LaunchMode.externalApplication);
       } else {
         Fluttertoast.showToast(
-            msg:
-                'Não foi possível redirecionar para a loja. Por favor, faça a atualização manualmente.',
+            msg: 'Não foi possível redirecionar.',
             toastLength: Toast.LENGTH_LONG,
             gravity: ToastGravity.BOTTOM,
             backgroundColor: Colors.grey);
@@ -63,169 +61,116 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.8,
-                child: PageView(
-                  scrollDirection: Axis.horizontal,
-                  controller: pageController,
-                  onPageChanged: (position) {
-                    if (mounted) {
-                      setState(() => currentIndexPage = position.toDouble());
-                    }
-                  },
+                height: 400,
+                child: Stack(
+                  alignment: Alignment.topCenter,
                   children: <Widget>[
-                    SingleChildScrollView(
-                      child: Stack(
-                        alignment: Alignment.topCenter,
-                        children: <Widget>[
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: circleRadius / 2.0),
-                            child: Container(
-                              //replace this Container with your Card
-                              color: Colors.white,
-                              height: 500.0,
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Expanded(
-                                        child: Column(children: <Widget>[
-                                      Container(
-                                        width: circleRadius,
-                                        height: circleRadius,
-                                        decoration: const ShapeDecoration(
-                                            shape: CircleBorder(),
-                                            color: Colors.white),
-                                        child: const Padding(
-                                          padding:
-                                              EdgeInsets.all(circleBorderWidth),
-                                          child: DecoratedBox(
-                                            decoration: ShapeDecoration(
-                                              shape: CircleBorder(),
-                                              image: DecorationImage(
-                                                fit: BoxFit.cover,
-                                                image: NetworkImage(
-                                                  'https://avatars.githubusercontent.com/u/17891980?v=4',
-                                                ),
-                                              ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: circleRadius / 2.0),
+                      child: Container(
+                        color: Colors.white,
+                        height: 400.0,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    width: circleRadius,
+                                    height: circleRadius,
+                                    decoration: const ShapeDecoration(
+                                        shape: CircleBorder(),
+                                        color: Colors.white),
+                                    child: const Padding(
+                                      padding:
+                                          EdgeInsets.all(circleBorderWidth),
+                                      child: DecoratedBox(
+                                        decoration: ShapeDecoration(
+                                          shape: CircleBorder(),
+                                          image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: NetworkImage(
+                                              'https://avatars.githubusercontent.com/u/17891980?v=4',
                                             ),
                                           ),
                                         ),
                                       ),
-                                      const Text('Wendreo Fernandes',
-                                          style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                          )),
-                                      Text('Software Developer',
-                                          style: TextStyle(
-                                              fontSize: 15,
-                                              color: Colors.grey[600])),
-                                      const SizedBox(height: 10),
-                                      // ElevatedButton(
-                                      //   onPressed: () {
-                                      //     debugPrint('ElevatedButton Clicked');
-                                      //   },
-                                      //   style: ElevatedButton.styleFrom(
-                                      //     backgroundColor: Colors.purple,
-                                      //     shape: RoundedRectangleBorder(
-                                      //       borderRadius:
-                                      //           BorderRadius.circular(10),
-                                      //     ),
-                                      //   ),
-                                      //   child: const Text(
-                                      //       'Visit my Github profile'),
-                                      // ),
-                                      const SizedBox(height: 10),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 12, right: 12, bottom: 12),
-                                        child: Text(
-                                          "I'm a software developer with +6 years of experience building software.\n\n"
-                                          "Expertise with:\n"
-                                          "• DevOps: Fastlane, Pipeline CI/CD, Docker, Portainer, GitHub Actions, GitLab CI, Git Hooks and deployment;\n"
-                                          "• Code Quality: Testing, SonarQube and CodeCov;\n"
-                                          "• Techs: Git, Flutter, SQL, C# (.NET Core/.NET 6.0, Web API and Worker Service) and VB.NET (.NET Framework and Windows Forms) and Scrum;\n"
-                                          "• Databases: SQL Server, Firebird, MySQL, SQLite and others.",
-                                          style: TextStyle(
-                                              fontSize: 13.5,
-                                              color: Colors.grey[600]),
-                                          textAlign: TextAlign.justify,
-                                        ),
-                                      ),
-                                      Text(
-                                          '#csharp #dotnet #flutter #devops #cicd',
-                                          style: TextStyle(
-                                              fontSize: 13.5,
-                                              color: Colors.grey[500])),
-                                      Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          IconButton(
-                                              icon: const FaIcon(
-                                                  FontAwesomeIcons.instagram),
-                                              onPressed: () {
-                                                launchURL(
-                                                    "https://www.instagram.com/wendreof/");
-                                              }),
-                                          IconButton(
-                                              icon: const FaIcon(
-                                                  FontAwesomeIcons.linkedinIn),
-                                              onPressed: () {
-                                                launchURL(
-                                                    "https://www.linkedin.com/in/wendreof/");
-                                              }),
-                                          IconButton(
-                                              icon: const FaIcon(
-                                                  FontAwesomeIcons.github),
-                                              onPressed: () {
-                                                launchURL(
-                                                    "https://github.com/wendreof");
-                                              }),
-                                        ],
-                                      )
-                                    ])),
-                                  ])
-                            ],
-                          ),
-                        ],
-                      ),
+                                    ),
+                                  ),
+                                  const Text('Wendreo Fernandes',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                  Text('Software Developer',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.grey[600])),
+                                  const SizedBox(height: 20),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 12, right: 12, bottom: 12),
+                                    child: Text(
+                                      "I'm a software developer with +6 years of experience building software.\n\n"
+                                      "Expertise with:\n"
+                                      "• DevOps: Fastlane, Pipeline CI/CD, Docker, Portainer, GitHub Actions, GitLab CI, Git Hooks and deployment;\n"
+                                      "• Code Quality: Testing, SonarQube and CodeCov;\n"
+                                      "• Techs: Git, Flutter, SQL, C# (.NET Core/.NET 6.0, Web API and Worker Service) and VB.NET (.NET Framework and Windows Forms) and Scrum;\n"
+                                      "• Databases: SQL Server, Firebird, MySQL, SQLite and others.",
+                                      style: TextStyle(
+                                          fontSize: 13.5,
+                                          color: Colors.grey[600]),
+                                      textAlign: TextAlign.justify,
+                                    ),
+                                  ),
+                                  Text(
+                                    '#csharp #dotnet #flutter #devops #cicd',
+                                    style: TextStyle(
+                                        fontSize: 13.5,
+                                        color: Colors.grey[500]),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      IconButton(
+                                          icon: const FaIcon(
+                                              FontAwesomeIcons.instagram),
+                                          onPressed: () {
+                                            launchURL(
+                                                "https://www.instagram.com/wendreof/");
+                                          }),
+                                      IconButton(
+                                          icon: const FaIcon(
+                                              FontAwesomeIcons.linkedinIn),
+                                          onPressed: () {
+                                            launchURL(
+                                                "https://www.linkedin.com/in/wendreof/");
+                                          }),
+                                      IconButton(
+                                          icon: const FaIcon(
+                                              FontAwesomeIcons.github),
+                                          onPressed: () {
+                                            launchURL(
+                                                "https://github.com/wendreof");
+                                          }),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                    SingleChildScrollView(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [],
-                          ),
-                        ])),
-                    SingleChildScrollView(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: const <Widget>[
-                          Text("3 hahaha"),
-                        ])),
                   ],
-                ),
-              ),
-              DotsIndicator(
-                dotsCount: 3,
-                position: currentIndexPage,
-                decorator: DotsDecorator(
-                  color: Theme.of(context).colorScheme.secondary,
-                  activeColor: Theme.of(context).primaryColor,
-                  size: const Size.square(9.0),
-                  activeSize: const Size(18.0, 9.0),
-                  activeShape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
                 ),
               ),
             ],
